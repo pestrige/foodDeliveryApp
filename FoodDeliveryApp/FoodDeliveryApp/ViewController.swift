299c1539beb6e4da4f79c5bae15b30989b8132e8
Dmitry Korovin <pestrige@gmail.com>
2024-02-04 09:25:35 +0300

    Create Xcode project

---
+++ FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  FoodDeliveryApp
+//
+//  Created by Dmitry on 04.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
